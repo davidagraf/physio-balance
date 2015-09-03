fc48ffac9bb42882f4503a80a164b6a971ae4a4c
David Graf <davidagraf@gmail.com>
2015-09-03 08:19:27 +0200

    setup webpack dev configuration

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  eslint: {
+    configFile: 'public/.eslintrc'
+  },
+  debug: true,
+  devtool: 'cheap-module-inline-source-map',
+  context: path.join(__dirname, 'public', 'scripts'),
+  entry: './main.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        include: __dirname,
+        exclude: path.join(__dirname, 'node_modules'),
+        loader: 'eslint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(
+          'css?sourceMap!autoprefixer'
+        )
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(
+          'css?sourceMap!autoprefixer!sass?outputStyle=expanded&sourceMap=true&sourceMapContents=true'
+        )
+      },
+      {
+        test: /\.js$/,
+        exclude: path.join(__dirname, 'node_modules'),
+        loader: 'babel?optional=runtime&blacklist=react'
+      },
+      { test: /\.json$/, loader: 'json-loader' },
+      {
+        test: /\.(ttf|svg|woff|eot)/,
+        loader: 'url-loader?limit=10000'
+      }
+    ],
+    noParse: [
+      new RegExp('jquery.min.js'),
+      new RegExp('bootstrap.min.js')
+    ]
+  },
+  plugins: [
+    // Extract inline css into separate 'style.css'
+    new ExtractTextPlugin('style.css'),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    }),
+  ],
+  resolve: {
+    alias: {
+      '^jquery$': 'jquery/dist/jquery.min.js',
+      '^bootstrap$': 'bootstrap-sass/assets/javascrpt/bootstrap.min.js'
+    }
+  }
+};
