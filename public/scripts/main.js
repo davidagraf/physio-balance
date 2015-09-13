70b580e3e9e2e88dc999630a03898754dc55c3fe
David Graf <davidagraf@gmail.com>
2015-09-13 14:56:28 +0200

    scrollspy & check if touch

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,6 +1,6 @@
 import 'styles/main.scss';
 import 'locales/de.l20n';
-import 'l20n/dist/webcompat/l20n.js';
+import l20n from 'l20n/dist/webcompat/l20n.js';
 import 'imgs/headerimg.png';
 
 const jquery = require('jquery');
@@ -17,3 +17,25 @@ jquery('#nav').affix({
     }
   }
 });
+
+const scrollspy = jquery('body').scrollspy({
+  target: '#navbar'
+});
+
+jquery(window).resize(() => scrollspy.scrollspy('refresh'));
+
+jquery(window.document).on('DOMLocalized', () => {
+  if (window.location.hash) {
+    document.getElementById(window.location.hash.substring(1)).scrollIntoView();
+  }
+  scrollspy.scrollspy('refresh');
+});
+
+jquery('header').on('click', '#navbar.in a', () => {
+  jquery('#nav .navbar-toggle').click();
+});
+
+
+if (!!('ontouchstart' in window)) {
+  jquery('body').addClass('touch');
+}
