eb7e83e5fe454cd49528ec43455d9d606e184a56
David Graf <davidagraf@gmail.com>
2015-10-15 21:43:58 +0200

    prototype schedule loading from google docs

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,12 +3,13 @@ import 'locales/de.l20n';
 import l20n from 'l20n/dist/webcompat/l20n.js';
 import 'imgs/headerimg.png';
 
+import 'scripts/schedule.js';
+
 const jquery = require('jquery');
+
 window.jQuery = jquery;
 window.$ = jquery;
 require('bootstrap-sass');
-
-//import 'bootstrap-sass';
 
 jquery('#nav').affix({
   offset: {
@@ -35,6 +36,7 @@ jquery('header').on('click', '#navbar.in a', () => {
   jquery('#nav .navbar-toggle').click();
 });
 
+// add touch class if on touch devices
 if (!!('ontouchstart' in window)) {
   jquery('body').addClass('touch');
 }
