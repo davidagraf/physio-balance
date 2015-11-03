8df244e85acbdb374dd298a75fb075097604c911
David Graf <davidagraf@gmail.com>
2015-11-03 08:10:49 +0100

    modal animation

--- public/scripts/components/modals/Modal.jsx
+++ public/scripts/components/modals/Modal.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
 import Icon from 'scripts/components/Icon.jsx';
 import {Emitter} from 'scripts/utils/events.js';
 
@@ -15,15 +17,21 @@ export default class extends React.Component {
   }
   render() {
     return (
-      <div className="modal__visible">
-        <div className="modal__header">
-          <div className="modal__header__title">{this.props.title}</div>
-          <button className="modal__header__close" onClick={() => Emitter.emit(this)}><Icon name="times" /></button>
+      <ReactCSSTransitionGroup transitionName="modal--animation"
+                               transitionAppear={true}
+                               transitionAppearTimeout={500}
+                               transitionEnterTimeout={0}
+                               transitionLeaveTimeout={0}>
+        <div className="modal__visible">
+          <div className="modal__header">
+            <div className="modal__header__title">{this.props.title}</div>
+            <button className="modal__header__close" onClick={() => Emitter.emit(this)}><Icon name="times" /></button>
+          </div>
+          <div className="modal__content">
+          { this.props.children }
+          </div>
         </div>
-        <div className="modal__content">
-        { this.props.children }
-        </div>
-      </div>
+      </ReactCSSTransitionGroup>
     );
   }
 }
