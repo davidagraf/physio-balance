50b4a923a39c8f1fbfe73ec17e8f908aef54997d
David Graf <davidagraf@gmail.com>
2015-11-04 19:22:13 +0100

    different team img

--- public/scripts/components/widgets/WidgetTeam.jsx
+++ public/scripts/components/widgets/WidgetTeam.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import Widget from 'scripts/components/widgets/Widget.jsx';
 import ModalTeam from 'scripts/components/modals/ModalTeam.jsx';
 
-import maxUndMoritz from 'imgs/maxundmoritz.jpg';
+import img from 'imgs/SMC-Managing-Teams.jpeg';
 
 export default class extends React.Component {
   render() {
     return (
-      <Widget img={maxUndMoritz} text={'Team'} modal={<ModalTeam />} />
+      <Widget img={img} text={'Team'} modal={<ModalTeam />} />
     );
   }
 }
