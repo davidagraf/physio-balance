01b4d122d2a9882f8f42aa1f05ab2964b32d4f3d
David Graf <davidagraf@gmail.com>
2015-11-12 07:44:09 +0100

    beautify widgets

--- public/scripts/components/widgets/WidgetContact.jsx
+++ public/scripts/components/widgets/WidgetContact.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import Widget from 'scripts/components/widgets/Widget.jsx';
 
+import contact from 'imgs/contact.jpg';
+
 export default class extends React.Component {
   render() {
     return (
-      <Widget text={'Kontakt'} url="https://docs.google.com/forms/d/19Sb_25eql6M0dpNbloSIcD4Q4r0Yp92XNjFlgsqwtJ8" />
+      <Widget
+        img={contact} text={'Kontakt'}
+        url="https://docs.google.com/forms/d/19Sb_25eql6M0dpNbloSIcD4Q4r0Yp92XNjFlgsqwtJ8" />
     );
   }
 }
--- public/scripts/components/widgets/WidgetTeam.jsx
+++ public/scripts/components/widgets/WidgetTeam.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Widget from 'scripts/components/widgets/Widget.jsx';
 import ModalTeam from 'scripts/components/modals/ModalTeam.jsx';
 
-import img from 'imgs/SMC-Managing-Teams.jpeg';
+import img from 'imgs/team.jpg';
 
 export default class extends React.Component {
   render() {
