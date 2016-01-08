6fbe57d16a2c7ad54a552a1818eba45aceab8c5a
David Graf <davidagraf@gmail.com>
2016-01-08 07:40:36 +0100

    enable wirbelsaeule

--- public/scripts/components/bars/BarPhysio.jsx
+++ public/scripts/components/bars/BarPhysio.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Bar from 'scripts/components/bars/Bar.jsx';
 
+import wirbelsaeule from 'imgs/wirbelsaeule.png';
+
 export default class extends React.Component {
   render() {
     return (
       <Bar
+        img={<img className="bar__img--physio" src={wirbelsaeule} />}
         title="Physio&shy;therapie"
         subtitle="&laquo;ihre Gesundheit ist mein Ziel&raquo;"
         className="bar--red"
