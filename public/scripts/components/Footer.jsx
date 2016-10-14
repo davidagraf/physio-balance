dbb01627eb314b3a04572dd8c1ea64687c932e18
David Graf <davidagraf@gmail.com>
2016-10-14 15:59:12 +0200

    centered footer text

--- public/scripts/components/Footer.jsx
+++ public/scripts/components/Footer.jsx
@@ -4,7 +4,7 @@ export default class extends React.Component {
   render() {
     return (
       <div className="footers">
-        <div className="footers__inner">
+        <div className="footers__inner text--center">
           &laquo;leben ist bewegung, bewegung ist gesundheit, gesundheit ist leben&raquo;
         </div>
       </div>
