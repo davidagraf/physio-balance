559bcf93d7f06ff8abf14f401f377e23c56e3019
David Graf <davidagraf@gmail.com>
2016-10-14 14:46:36 +0200

    footer

--- public/scripts/components/Footer.jsx
+++ public/scripts/components/Footer.jsx
@@ -4,7 +4,9 @@ export default class extends React.Component {
   render() {
     return (
       <div className="footers">
-        <div className="footers__inner" />
+        <div className="footers__inner">
+          &laquo;Leben ist Bewegung, Bewegung ist Gesundheit, Gesundheit ist Leben&raquo;
+        </div>
       </div>
     );
   }
